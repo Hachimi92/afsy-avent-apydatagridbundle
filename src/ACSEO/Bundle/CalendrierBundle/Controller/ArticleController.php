4a2ecb4c500853978bd89410109a052df49b3fd6
Nicolas Potier <nicolas.potier@acseo-conseil.fr>
2013-12-02 16:55:14 +0100

    Ajout d'une colonne Auteur ainsi que des boutons d'actions

--- src/ACSEO/Bundle/CalendrierBundle/Controller/ArticleController.php
+++ src/ACSEO/Bundle/CalendrierBundle/Controller/ArticleController.php
@@ -11,6 +11,8 @@ use ACSEO\Bundle\CalendrierBundle\Entity\Article;
 
 // Inclusion de la classe Entity d'APYDataGrid
 use APY\DataGridBundle\Grid\Source\Entity;
+use APY\DataGridBundle\Grid\Column\BlankColumn;
+use APY\DataGridBundle\Grid\Action\RowAction;
 
 /**
  * Article controller.
@@ -38,7 +40,22 @@ class ArticleController extends Controller
 
         // Affectation de la source 
         $grid->setSource($source);
+
+        // Manipulation des données pour renseigner la colonne Auteur
+        $source->manipulateRow(
+            function ($row) {
+                $row->setField('author', $row->getField("writer.firstName")." ".$row->getField("writer.lastName"));
+
+                return $row;
+        });
         
+        $rowAction = new RowAction("Voir l'article", 'article_show');
+        $grid->addRowAction($rowAction);
+        
+        $rowAction = new RowAction("Supprimer l'article", 'article_delete', true, '_self');
+        $rowAction->setConfirmMessage("Etes vous sur de vouloir supprimer cet article ?");
+        $grid->addRowAction($rowAction);
+
         // Renvoie une réponse
         return $grid->getGridResponse('ACSEOCalendrierBundle:Article:index.html.twig');
     }
@@ -63,5 +80,17 @@ class ArticleController extends Controller
         return array(
             'entity'      => $entity,
         );
+    }
+
+        /**
+     * Finds and displays a Article entity.
+     *
+     * @Route("/{id}", name="article_delete")
+     * @Method("GET")
+     * @Template()
+     */
+    public function deleteAction($id)
+    {
+       return array();
     }
 }
